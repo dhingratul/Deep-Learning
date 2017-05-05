93d94136692f6003099a4777d04732c9a202c8c0
dhingratul <atul.dhingra@rutgers.edu>
2017-05-05 10:35:19 -0400

    Added en edge case

--- Softmax.py
+++ Softmax.py
@@ -14,7 +14,7 @@ scores = [3.0, 1.0, 0.2]
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
-    return np.exp(x)/np.sum(np.exp(x))
+     return np.exp(x)/np.sum(np.exp(x),axis = 0)
 
 print(softmax(scores))
 
