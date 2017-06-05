6138a62951df563ddf9c171f49d85a0f94a7f15c
dhingratul <atul.dhingra@rutgers.edu>
2017-06-05 13:48:28 -0400

    Link to P-6

--- 1_notmnist.py
+++ 1_notmnist.py
@@ -15,7 +15,6 @@ import sys
 import tarfile
 from IPython.display import display, Image
 from scipy import ndimage
-from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
 from random import randint
